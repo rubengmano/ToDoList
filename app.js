996ce3a72cebf3953ab4b565c301c11f8996e60a
Rúben Mano <rumcmano@gmail.com>
2019-11-02 17:09:02 +0200

    Understanding templating vs Layouts

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ const app = express();
 // first is needed to install ejs module
 app.set('view engine', 'ejs');
 // Set the body parser to be abled to read
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 // Define the path to public files
 app.use(express.static('public'));
 
 // because of scope it's necessary to declare the variable at a global level
 let items = ['Buy Food', 'Cook Food', 'Eat Food'];
+let workItems = [];
 
 app.get("/", (req, res) => {
   let today = new Date();
@@ -30,14 +33,34 @@ app.get("/", (req, res) => {
   let day = today.toLocaleDateString("en-US", options);
 
   // it's necessary to have a views folder and the file inside
-  res.render('index', {weekDay: day, newListItems: items});
+  res.render('index', {
+    ListTitle: day,
+    newListItems: items
+  });
 });
 
 app.post("/", (req, res) => {
-  var newItem = req.body.newItem;
-  items.push(newItem);
-  // It's redirect to the get method to complete the ejs file
-  res.redirect('/');
+
+  let newItem = req.body.newItem;
+
+  console.log(req.body.list);
+
+  if (req.body.list === 'Work') {
+    workItems.push(newItem);
+    res.redirect('/work');
+  } else {
+    items.push(newItem);
+    // It's redirect to the get method to complete the ejs file
+    res.redirect('/');
+  }
 });
+
+app.get('/work', (req, res) => {
+  res.render('index', {
+    ListTitle: 'Work List',
+    newListItems: workItems
+  });
+});
+
 
 app.listen(3000, () => console.log(`Server is up and running on port 3000.`));
